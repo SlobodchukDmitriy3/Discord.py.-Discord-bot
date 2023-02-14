3be4066fe6d9f6fca8bbe11c35e9e649d573a928
Дмитрий Слободчук <104769679+SlobodchukDmitriy3@users.noreply.github.com>
2023-02-14 22:43:25 +0200

    Add files via upload

---
+++ Telebot.py
@@ -0,0 +1,10 @@
+import discord
+class MyClient(discord.Client):
+    async def Klubnika(self):
+        print('Logged on as {0}!'.format(self.user))
+        async def Calc(self,message):
+            print('Message from {0.autor: {0.content}'.format(message))
+client = MyClient()
+client.run('MTA3NDAxMzkxOTA0MTAzNjM1OA.G9XXzL.v7qP1EH1Gc_j46lSW2SUfMlGIWmEF1QvqojDI4')
+
+    
